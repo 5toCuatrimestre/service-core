06ebdce1a21cf1dcddf5fe8b38b93a847cb1ef38
Isaacjbar <isaac777jb@gmail.com>
2025-01-15 08:01:12 -0600

    Second commit

---
+++ src/test/java/jbar/service_core/ServiceCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package jbar.service_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
