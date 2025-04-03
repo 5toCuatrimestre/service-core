e2fa0b03593068eecc8a084c82cc663879660604
Daniel <20233tn124@utez.edu.mx>
2025-04-03 15:10:04 -0600

    xde

--- src/main/java/jbar/service_core/Util/Config/WebConfig.java
+++ src/main/java/jbar/service_core/Util/Config/WebConfig.java
@@ -1,17 +1,23 @@
-package jbar.service_core.Util.Config;
+  package jbar.service_core.Util.Config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+  import org.springframework.context.annotation.Configuration;
+  import org.springframework.web.servlet.config.annotation.CorsRegistry;
+  import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-  @Override
-  public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/**")
-      .allowedOrigins("http://localhost:5173") // Sin la barra final
-      .allowedMethods("GET", "POST", "PUT", "DELETE")
-      .allowedHeaders("Authorization", "Content-Type", "Accept")
-      .allowCredentials(true);
+  @Configuration
+  public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+      registry.addMapping("/**")
+              .allowedOrigins(
+                      "http://localhost:3000",
+                      "http://localhost:8081",
+                      "http://localhost:8080",
+                      "http://localhost:5000" // Añade esto si es necesario
+              )
+              .allowedMethods("*") // Permite todos los métodos
+              .allowedHeaders("*") // Permite todos los headers
+              .allowCredentials(true)
+              .maxAge(3600); // Cache de opciones CORS por 1 hora
+    }
   }
-}
